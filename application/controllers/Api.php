543b34c39c5296ae1fd44f1f77088763f685a294
dempuldev <1903040037@ump.ac.id>
2022-07-06 12:30:24 +0700

    add little changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -17,18 +17,22 @@ class Api extends CI_Controller {
         // $this->load->view('v_supply');
         $wsdl  = base_url("/index.php?wsdl");
         $client = new nusoap_client($wsdl, true);
+
+        // Check for an error
         $err = $client->getError();
         if ($err) {
-            return false;
+            // Display the error
+            echo '<h2>Constructor error</h2><pre>' . $err . '</pre>';
+            // At this point, you know the call that follows will fail
         }
 
         $result = $client->call('supply');
 
         // Check for a fault
-    if ($client->fault) {
-        echo '<h2>Fault</h2><pre>';
-        print_r($result);
-        echo '</pre>';
+        if ($client->fault) {
+            echo '<h2>Fault</h2><pre>';
+            print_r($result);
+            echo '</pre>';
         } else {
             // Check for errors
             $err = $client->getError();
@@ -37,13 +41,18 @@ class Api extends CI_Controller {
                 echo '<h2>Error</h2><pre>' . $err . '</pre>';
             } else {
                 // Display the result
-                // echo '<h2>Result</h2><pre>';
-                // echo number_format((float) $result, 2, '.', ''); 
-                $format = sprintf("%01.4f", $result);
-                echo $format;
+                $response = sprintf("%01.4f", $result);
+                echo $response;
 
             }
+            // $response = sprintf("%01.4f", $result);
+            // echo $response;
         }
+        // Display the request and response
+        // echo '<h2>Request</h2>';
+        // echo '<pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
+        // echo '<h2>Response</h2>';
+        // echo '<pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
     }
 
     // Direct menuju halaman total supply
@@ -51,18 +60,21 @@ class Api extends CI_Controller {
         // $this->load->view('v_totalsupply');
         $wsdl  = base_url("/index.php?wsdl");
         $client = new nusoap_client($wsdl, true);
-        $err = $client->getError();
-        if ($err) {
-            return false;
-        }
+        // $err = $client->getError();
+        // if ($err) {
+        //     return false;
+        // }
 
         $result = $client->call('total_supply');
 
         // Check for a fault
         if ($client->fault) {
-            echo '<h2>Fault</h2><pre>';
-            print_r($result);
-            echo '</pre>';
+            // echo '<h2>Fault</h2><pre>';
+            // print_r($result);
+            // echo '</pre>';
+
+            echo "Fault: <p> Code: (". $client->faultcode .")</p> ";
+            echo "String: " . $client->faultstring;
             } else {
                 // Check for errors
                 $err = $client->getError();
@@ -72,10 +84,13 @@ class Api extends CI_Controller {
                 } else {
                     // Display the result
                     // echo '<h2>Result</h2><pre>';
-                    // echo number_format((float) $result, 2, '.', '');
-                    $format = sprintf("%01.4f", $result);
-                    echo $format; 
+                    // echo number_format((float) $result, 2, '.', ''); 
+                    $response = sprintf("%01.4f", $result);
+                    echo $response;
+
                 }
+                // $response = sprintf("%01.4f", $result);
+                // echo $response;
             }
         }
     }
